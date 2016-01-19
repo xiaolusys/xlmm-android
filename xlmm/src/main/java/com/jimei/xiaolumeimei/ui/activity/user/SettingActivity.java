577e74c7840b62f5cfcf95e90c4bc76eb92fe770
itxuye <itxuye@gmail.com>
2016-01-19 09:09:03 +0800

    提交新的代码

---
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/user/SettingActivity.java
@@ -0,0 +1,9 @@
+package com.jimei.xiaolumeimei.ui.activity.user;
+
+/**
+ * Created by itxuye(www.itxuye.com) on 2016/01/18.
+ *
+ * Copyright 2015年 上海己美. All rights reserved.
+ */
+public class SettingActivity {
+}
