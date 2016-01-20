33dd4ccf1db581b61d11c0fe7b2ddb86a51a34a4
wulei <crackwl@163.com>
2016-01-20 20:18:33 +0800

    设置信息代码提交

---
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/main/CompanyInfoActvity.java
@@ -0,0 +1,62 @@
+package com.jimei.xiaolumeimei.ui.activity.main;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import com.jimei.xiaolumeimei.R;
+import com.jimei.xiaolumeimei.base.BaseSwipeBackCompatActivity;
+import com.jimei.xiaolumeimei.entities.AddressResultBean;
+import com.jimei.xiaolumeimei.model.UserModel;
+import com.jimei.xiaolumeimei.xlmmService.ServiceResponse;
+import com.jude.utils.JUtils;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by itxuye(www.itxuye.com) on 2016/01/19.
+ *
+ * Copyright 2015年 上海己美. All rights reserved.
+ */
+public class CompanyInfoActvity extends BaseSwipeBackCompatActivity {
+  
+ 
+
+  @Override protected void setListener() {
+    
+  }
+
+  @Override protected void initData() {
+
+  }
+
+  @Override protected void getBundleExtras(Bundle extras) {
+
+  }
+
+  @Override protected int getContentViewLayoutID() {
+    return R.layout.companyinfo_activity;
+  }
+
+  @Override protected void initViews() {
+
+  }
+
+  @Override protected boolean toggleOverridePendingTransition() {
+    return false;
+  }
+
+  @Override protected TransitionMode getOverridePendingTransitionMode() {
+    return null;
+  }
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+   
+  }
+
+  
+  
+}
