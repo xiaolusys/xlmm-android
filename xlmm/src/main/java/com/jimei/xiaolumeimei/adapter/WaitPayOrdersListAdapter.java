89049c1309967b166a4a50bc21dccad87c6df480
wulei <crackwl@163.com>
2016-01-21 21:43:44 +0800

    优惠券积分代码提交

--- xlmm/src/main/java/com/jimei/xiaolumeimei/adapter/WaitPayOrdersListAdapter.java
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/adapter/WaitPayOrdersListAdapter.java
@@ -56,7 +56,7 @@ public class WaitPayOrdersListAdapter extends BaseAdapter {
     public void update(List<AllOrdersBean.ResultsEntity> list) {
 
         float payment = 0;
-        int orderState = 0;
+        String orderState = "";
 
 
 
@@ -64,11 +64,11 @@ public class WaitPayOrdersListAdapter extends BaseAdapter {
         for (int i = 0; i < list.size(); i++) {
             HashMap<String, String> map = new HashMap<String, String>();
             payment = (float)list.get(i).getPayment();
-            orderState = list.get(i).getStatus();
+            orderState = list.get(i).getStatusDisplay();
 
 
             map.put("payment", Float.toString(payment) );
-            map.put("orderState", Integer.toString(orderState) );
+            map.put("orderState", orderState );
 
             data.add(map);
         }
@@ -108,7 +108,7 @@ public class WaitPayOrdersListAdapter extends BaseAdapter {
         tx_order_sate = (TextView) convertView.findViewById(R.id.tx_order_state);
 
         tx_payment.setText("实付金额"+data.get(position).get("payment"));
-        tx_order_sate.setText("交易状态"+data.get(position).get("orderState"));
+        tx_order_sate.setText(data.get(position).get("orderState"));
 
         LinearLayout llayout = (LinearLayout) convertView.findViewById(R.id.llayout_order_item);
         if(1 == mList.get(position).getOrders().size()) {
