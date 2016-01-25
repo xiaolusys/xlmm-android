928674d6ec51e18750f85766afc0f6c96786fa88
itxuye <itxuye@gmail.com>
2016-01-25 14:23:11 +0800

    布局修改

---
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/product/SizeActivity.java
@@ -0,0 +1,9 @@
+package com.jimei.xiaolumeimei.ui.activity.product;
+
+/**
+ * Created by itxuye(www.itxuye.com) on 2016/01/25.
+ *
+ * Copyright 2015年 上海己美. All rights reserved.
+ */
+public class SizeActivity {
+}
