21008a51faadcd54e2ef4fcdd9a5fc6a1992980c
lei.wu <crackwl@163.com>
2016-01-28 16:59:53 +0800

    订单信息美化

--- xlmm/src/main/java/com/jimei/xiaolumeimei/adapter/AllOrdersListAdapter.java
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/adapter/AllOrdersListAdapter.java
@@ -101,92 +101,69 @@ public class AllOrdersListAdapter extends BaseAdapter {
     TextView tx_payment = null;
     TextView tx_order_sate = null;
     ViewHolder holder = null;
+    LinearLayout llayout;
     final int order_id = mList.get(position).getId();
+    Log.d(TAG, "order_id "+order_id );
 
     if (convertView == null) {
+      Log.d(TAG, "convertView == null " );
       convertView = LayoutInflater.from(context).inflate(R.layout.orders_list_item, null);
       holder = new ViewHolder();
 
       //这个地方比较奇葩，只有1个商品时显示商品详细信息，多余1个商品时只显示商品图片
-      LinearLayout llayout =
-          (LinearLayout) convertView.findViewById(R.id.llayout_order_item);
+      llayout = (LinearLayout) convertView.findViewById(R.id.llayout_order_item);
 
       if (1 == mList.get(position).getOrders().size()) {
-        LayoutInflater inflater = LayoutInflater.from(context);
-        LinearLayout ll_one_order =
-            (LinearLayout) inflater.inflate(R.layout.one_order_item, null);
-        ll_one_order.setOnClickListener(new View.OnClickListener() {
-
-          @Override public void onClick(View v) {
-            // TODO Auto-generated method stub
-            Log.d(TAG, "onClick ");
-            Intent intent = new Intent(context, OrderDetailActivity.class);
-            intent.putExtra("orderinfo", order_id);
-            Log.d(TAG, "transfer orderid  " + order_id + " to OrderDetailActivity");
-            context.startActivity(intent);
-          }
-        });
-
-        ImageView img_goods = (ImageView) ll_one_order.findViewById(R.id.img_good);
-        TextView tx_good_name = (TextView) ll_one_order.findViewById(R.id.tx_good_name);
-        TextView tx_good_price = (TextView) ll_one_order.findViewById(R.id.tx_good_price);
-        TextView tx_good_size = (TextView) ll_one_order.findViewById(R.id.tx_good_size);
-        TextView tx_good_num = (TextView) ll_one_order.findViewById(R.id.tx_good_num);
-
-        if(mList.get(position).getOrders().get(0).getTitle().length() >= 11) {
-          tx_good_name.setText(mList.get(position).getOrders().get(0).getTitle().substring(0, 10) + "...");
-        }
-        else {
-          tx_good_name.setText(mList.get(position).getOrders().get(0).getTitle());
-        }
-        tx_good_price.setText("¥" + mList.get(position).getOrders().get(0).getPayment());
-        tx_good_size.setText("尺码:" + mList.get(position).getOrders().get(0).getSkuName());
-        tx_good_num.setText(
-            "x" + Integer.toString(mList.get(position).getOrders().get(0).getNum()));
-
-        ViewUtils.loadImgToImgView(context, img_goods, data.get(position).get("img_url"));
-
+        LinearLayout ll_one_order = crt_one_goods_view(position, order_id);
         llayout.addView(ll_one_order);
+        holder.goods_num = 1;
+
         holder.ll_goods = ll_one_order;
       } else {
-        List<String> mDatas = new ArrayList<String>();
-        fillPicPath(mDatas, mList.get(position).getOrders());
-        Log.d(TAG,
-            "goodsnum " + mList.get(position).getOrders().size() + " " + mDatas.size());
-
-        LayoutInflater inflater = LayoutInflater.from(context);
-        MyHorizontalScrollView mHorizontalScrollView =
-            (MyHorizontalScrollView) inflater.inflate(R.layout.hscrollview_gallery, null);
-        HorizontalScrollViewAdapter mAdapter =
-            new HorizontalScrollViewAdapter(context, mDatas);
-        //添加滚动回调
-        mHorizontalScrollView.setCurrentImageChangeListener(
-            new MyHorizontalScrollView.CurrentImageChangeListener() {
-              @Override
-              public void onCurrentImgChanged(int position, View viewIndicator) {
-
-              }
-            });
-        //添加点击回调
-        mHorizontalScrollView.setOnItemClickListener(
-            new MyHorizontalScrollView.OnItemClickListener() {
-
-              @Override public void onClick(View view, int position) {
-                Intent intent = new Intent(context, OrderDetailActivity.class);
-                intent.putExtra("orderinfo", order_id);
-                Log.d(TAG, "transfer orderid  " + order_id + " to OrderDetailActivity");
-                context.startActivity(intent);
-              }
-            });
-        //设置适配器
-        mHorizontalScrollView.initDatas(mAdapter);
+        MyHorizontalScrollView mHorizontalScrollView = crt_more_goods_view(position, order_id);
         llayout.addView(mHorizontalScrollView);
+        holder.goods_num = mList.get(position).getOrders().size();
         holder.mHorizontalScrollView = mHorizontalScrollView;
       }
+      holder.position = position;
+
       convertView.setTag(holder);
     } else {
       holder = (ViewHolder) convertView.getTag();
+      llayout = (LinearLayout) convertView.findViewById(R.id.llayout_order_item);
+
+      if(position == holder.position){
+        Log.d(TAG, "same position update,do nothing " );
+      }
+      else {
+        Log.d(TAG, "NOT same position update,CONVERT " );
+        if(1== holder.goods_num){
+          holder.ll_goods.setVisibility(View.GONE);
+          Log.d(TAG, "ll_goods gone " );
+        }
+        else {
+          holder.mHorizontalScrollView.setVisibility(View.GONE);
+          Log.d(TAG, "mHorizontalScrollView gone " );
+        }
+
+        if (1 == mList.get(position).getOrders().size()) {
+          LinearLayout ll_one_order = crt_one_goods_view(position, order_id);
+          llayout.addView(ll_one_order);
+          holder.goods_num = 1;
+          holder.ll_goods = ll_one_order;
+        } else {
+          MyHorizontalScrollView mHorizontalScrollView = crt_more_goods_view(position, order_id);
+          llayout.addView(mHorizontalScrollView);
+          holder.goods_num = mList.get(position).getOrders().size();
+          holder.mHorizontalScrollView = mHorizontalScrollView;
+        }
+        holder.position = position;
+
+        convertView.setTag(holder);
+      }
     }
+
+
 
     tx_order_sate = (TextView) convertView.findViewById(R.id.tx_order_state);
 
@@ -242,9 +219,90 @@ public class AllOrdersListAdapter extends BaseAdapter {
     return left;
   }
 
+  private LinearLayout crt_one_goods_view(int position, int order_id) {
+    Log.d(TAG, "crt_one_goods_view ");
+
+    LayoutInflater inflater = LayoutInflater.from(context);
+    LinearLayout ll_one_order =
+            (LinearLayout) inflater.inflate(R.layout.one_order_item, null);
+    ll_one_order.setOnClickListener(new View.OnClickListener() {
+
+      @Override public void onClick(View v) {
+        // TODO Auto-generated method stub
+        Log.d(TAG, "onClick ");
+        Intent intent = new Intent(context, OrderDetailActivity.class);
+        intent.putExtra("orderinfo", order_id);
+        Log.d(TAG, "LinearLayout transfer orderid  " + order_id + " to OrderDetailActivity");
+        context.startActivity(intent);
+      }
+    });
+
+    ImageView img_goods = (ImageView) ll_one_order.findViewById(R.id.img_good);
+    TextView tx_good_name = (TextView) ll_one_order.findViewById(R.id.tx_good_name);
+    TextView tx_good_price = (TextView) ll_one_order.findViewById(R.id.tx_good_price);
+    TextView tx_good_size = (TextView) ll_one_order.findViewById(R.id.tx_good_size);
+    TextView tx_good_num = (TextView) ll_one_order.findViewById(R.id.tx_good_num);
+
+    if(mList.get(position).getOrders().get(0).getTitle().length() >= 11) {
+      tx_good_name.setText(mList.get(position).getOrders().get(0).getTitle().substring(0, 10) + "...");
+    }
+    else {
+      tx_good_name.setText(mList.get(position).getOrders().get(0).getTitle());
+    }
+    tx_good_price.setText("¥" + mList.get(position).getOrders().get(0).getPayment());
+    tx_good_size.setText("尺码:" + mList.get(position).getOrders().get(0).getSkuName());
+    tx_good_num.setText(
+            "x" + Integer.toString(mList.get(position).getOrders().get(0).getNum()));
+
+    ViewUtils.loadImgToImgView(context, img_goods, data.get(position).get("img_url"));
+
+    return ll_one_order;
+  }
+
+  private MyHorizontalScrollView crt_more_goods_view(int position, int order_id) {
+    Log.d(TAG, "crt_more_goods_view ");
+
+    List<String> mDatas = new ArrayList<String>();
+    fillPicPath(mDatas, mList.get(position).getOrders());
+    Log.d(TAG,
+            "goodsnum " + mList.get(position).getOrders().size() + " " + mDatas.size());
+
+    LayoutInflater inflater = LayoutInflater.from(context);
+    MyHorizontalScrollView mHorizontalScrollView =
+            (MyHorizontalScrollView) inflater.inflate(R.layout.hscrollview_gallery, null);
+    HorizontalScrollViewAdapter mAdapter =
+            new HorizontalScrollViewAdapter(context, mDatas);
+    //添加滚动回调
+    mHorizontalScrollView.setCurrentImageChangeListener(
+            new MyHorizontalScrollView.CurrentImageChangeListener() {
+              @Override
+              public void onCurrentImgChanged(int position, View viewIndicator) {
+
+              }
+            });
+    //添加点击回调
+    mHorizontalScrollView.setOnItemClickListener(
+            new MyHorizontalScrollView.OnItemClickListener() {
+
+              @Override public void onClick(View view, int position) {
+                Intent intent = new Intent(context, OrderDetailActivity.class);
+                intent.putExtra("orderinfo", order_id);
+                Log.d(TAG, "mHorizontalScrollView transfer orderid  " + order_id + " to OrderDetailActivity");
+                context.startActivity(intent);
+              }
+            });
+    //设置适配器
+    mHorizontalScrollView.initDatas(mAdapter);
+
+    return mHorizontalScrollView;
+  }
+
   public static class ViewHolder {
-    public LinearLayout ll_goods;
+    int goods_num;
+    int position;
+    LinearLayout ll_goods;
     MyHorizontalScrollView mHorizontalScrollView;
+
   }
 }
 
