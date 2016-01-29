67aa25dfc404ffce2f06c2ecc89ea903c1da1729
itxuye <itxuye@gmail.com>
2016-01-29 13:10:27 +0800

    提交支付bug修改,toolbar字体修改

--- xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/trade/CartActivity.java
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/trade/CartActivity.java
@@ -2,24 +2,35 @@ package com.jimei.xiaolumeimei.ui.activity.trade;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import butterknife.Bind;
+import butterknife.ButterKnife;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.jimei.xiaolumeimei.R;
-import com.jimei.xiaolumeimei.adapter.CartsAdapetr;
 import com.jimei.xiaolumeimei.base.BaseSwipeBackCompatActivity;
+import com.jimei.xiaolumeimei.entities.CartsPayinfoBean;
 import com.jimei.xiaolumeimei.entities.CartsinfoBean;
 import com.jimei.xiaolumeimei.model.CartsModel;
 import com.jimei.xiaolumeimei.ui.activity.main.MainActivity;
+import com.jimei.xiaolumeimei.utils.ViewUtils;
 import com.jimei.xiaolumeimei.widget.DividerItemDecoration;
 import com.jimei.xiaolumeimei.widget.headerandfooterrecyclerview.HeaderAndFooterRecyclerViewAdapter;
 import com.jimei.xiaolumeimei.widget.headerandfooterrecyclerview.RecyclerViewUtils;
 import com.jimei.xiaolumeimei.xlmmService.ServiceResponse;
 import com.jude.utils.JUtils;
+import com.squareup.okhttp.ResponseBody;
+import com.zhy.autolayout.utils.AutoUtils;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import rx.schedulers.Schedulers;
@@ -36,44 +47,72 @@ public class CartActivity extends BaseSwipeBackCompatActivity
   @Bind(R.id.carts_recyclerview) RecyclerView cartsRecyclerview;
   @Bind(R.id.confirm) Button confirmTrade;
   @Bind(R.id.total_price) TextView totalPrice;
+  private TextView totalPrice_all_1;
+  private  TextView extra_price_a;
   @Bind(R.id.go_main) Button goMain;
   @Bind(R.id.empty_content) RelativeLayout emptyContent;
   private CartsAdapetr mCartsAdapetr;
   private HeaderAndFooterRecyclerViewAdapter mHeaderAndFooterRecyclerViewAdapter;
+  private double total_price;
+  private List<CartsinfoBean> mList;
+  private double extra_price;
+ private View view;
 
   @Override protected void setListener() {
     confirmTrade.setOnClickListener(this);
   }
 
   @Override protected void initData() {
-    //model.getCartsList()
-    //    .subscribeOn(Schedulers.io())
-    //    .subscribe(new ServiceResponse<List<CartsinfoBean>>() {
-    //      @Override public void onNext(List<CartsinfoBean> cartsinfoBeans) {
-    //
-    //        if ((cartsinfoBeans != null) && (cartsinfoBeans.size() > 0)) {
-    //          View view = getLayoutInflater().inflate(R.layout.footer, null);
-    //          RecyclerViewUtils.setFooterView(cartsRecyclerview, view);
-    //          mCartsAdapetr.update(cartsinfoBeans);
-    //          //double total_price = 0;
-    //          for (int i = 0; i < cartsinfoBeans.size(); i++) {
-    //            ids.add(cartsinfoBeans.get(i).getId());
-    //            JUtils.Log("ITXUYE", cartsinfoBeans.toString());
-    //          }
-    //        } else {
-    //          RecyclerViewUtils.removeFooterView(cartsRecyclerview);
-    //          emptyContent.setVisibility(View.VISIBLE);
-    //          goMain.setOnClickListener(new View.OnClickListener() {
-    //            @Override public void onClick(View v) {
-    //              startActivity(new Intent(CartActivity.this, MainActivity.class));
-    //              finish();
-    //            }
-    //          });
-    //        }
-    //      }
-    //    });
-    //
-    //totalPrice.setText(mCartsAdapetr.total_price + "");
+    model.getCartsList()
+        .subscribeOn(Schedulers.io())
+        .subscribe(new ServiceResponse<List<CartsinfoBean>>() {
+          @Override public void onNext(List<CartsinfoBean> cartsinfoBeans) {
+
+            if ((cartsinfoBeans != null) && (cartsinfoBeans.size() > 0)) {
+
+              RecyclerViewUtils.setFooterView(cartsRecyclerview, view);
+              mCartsAdapetr.update(cartsinfoBeans);
+              for (int i = 0; i < cartsinfoBeans.size(); i++) {
+                ids.add(cartsinfoBeans.get(i).getId());
+              }
+
+              StringBuilder sb = new StringBuilder();
+              String s = null;
+              if (ids.size() > 0) {
+                s = apendString(sb);
+              }
+
+              model.getCartsInfoList(s)
+                  .subscribeOn(Schedulers.newThread())
+                  .subscribe(new ServiceResponse<CartsPayinfoBean>() {
+                    @Override public void onNext(CartsPayinfoBean cartsPayinfoBean) {
+                      super.onNext(cartsPayinfoBean);
+                      if (cartsPayinfoBean != null) {
+
+                        total_price = cartsPayinfoBean.getTotalFee();
+                        totalPrice.setText("¥" + total_price);
+                        totalPrice_all_1.setText("总金额¥" + total_price);
+                        if (total_price < 150) {
+                          extra_price = 150 - total_price;
+                          extra_price_a.setText(extra_price + "");
+                        } else {
+                          extra_price_a.setText(0 + "");
+                        }
+                      }
+                    }
+                  });
+            } else {
+              //RecyclerViewUtils.removeFooterView(cartsRecyclerview);
+              emptyContent.setVisibility(View.VISIBLE);
+              goMain.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                  startActivity(new Intent(CartActivity.this, MainActivity.class));
+                  finish();
+                }
+              });
+            }
+          }
+        });
   }
 
   @Override protected void getBundleExtras(Bundle extras) {
@@ -85,11 +124,18 @@ public class CartActivity extends BaseSwipeBackCompatActivity
   }
 
   @Override protected void initViews() {
+
+    view = getLayoutInflater().inflate(R.layout.footer, null);
+
+    totalPrice_all_1 = (TextView) view.findViewById(R.id.total_price_all_1);
+    extra_price_a = (TextView) view.findViewById(R.id.extra_price_a);
+
+
     cartsRecyclerview.setLayoutManager(new LinearLayoutManager(this));
 
     cartsRecyclerview.addItemDecoration(
         new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
-    mCartsAdapetr = new CartsAdapetr(this);
+    mCartsAdapetr = new CartsAdapetr();
     mHeaderAndFooterRecyclerViewAdapter =
         new HeaderAndFooterRecyclerViewAdapter(mCartsAdapetr);
     cartsRecyclerview.setAdapter(mHeaderAndFooterRecyclerViewAdapter);
@@ -109,12 +155,7 @@ public class CartActivity extends BaseSwipeBackCompatActivity
         Intent intent = new Intent(CartActivity.this, CartsPayInfoActivity.class);
         StringBuilder sb = new StringBuilder();
         if (ids.size() > 0) {
-          for (int i = 0; i < ids.size(); i++) {
-            sb.append(ids.get(i)).append(",");
-          }
-          String str = new String(sb);
-
-          String s = str.substring(0, str.length() - 1);
+          String s = apendString(sb);
 
           intent.putExtra("ids", s);
           JUtils.Log("CartActivity", s);
@@ -126,35 +167,226 @@ public class CartActivity extends BaseSwipeBackCompatActivity
     }
   }
 
-  @Override protected void onResume() {
-    super.onResume();
-    model.getCartsList()
-        .subscribeOn(Schedulers.io())
-        .subscribe(new ServiceResponse<List<CartsinfoBean>>() {
-          @Override public void onNext(List<CartsinfoBean> cartsinfoBeans) {
+  @NonNull private String apendString(StringBuilder sb) {
+    for (int i = 0; i < ids.size(); i++) {
+      sb.append(ids.get(i)).append(",");
+    }
+    String str = new String(sb);
 
-            if ((cartsinfoBeans != null) && (cartsinfoBeans.size() > 0)) {
-              View view = getLayoutInflater().inflate(R.layout.footer, null);
-              RecyclerViewUtils.setFooterView(cartsRecyclerview, view);
-              mCartsAdapetr.update(cartsinfoBeans);
-              //double total_price = 0;
-              for (int i = 0; i < cartsinfoBeans.size(); i++) {
-                ids.add(cartsinfoBeans.get(i).getId());
-                JUtils.Log("ITXUYE", cartsinfoBeans.toString());
-              }
-            } else {
-              RecyclerViewUtils.removeFooterView(cartsRecyclerview);
-              emptyContent.setVisibility(View.VISIBLE);
-              goMain.setOnClickListener(new View.OnClickListener() {
-                @Override public void onClick(View v) {
-                  startActivity(new Intent(CartActivity.this, MainActivity.class));
-                  finish();
+    return str.substring(0, str.length() - 1);
+  }
+
+  //内部Recycler Adapter类
+  class CartsAdapetr extends RecyclerView.Adapter<CartsAdapetr.CartsVH> {
+
+    public CartsAdapetr() {
+      mList = new ArrayList<>();
+    }
+
+    @Override public CartsVH onCreateViewHolder(ViewGroup parent, int viewType) {
+
+      View view;
+
+      view = LayoutInflater.from(parent.getContext())
+          .inflate(R.layout.item_carts, parent, false);
+      AutoUtils.autoSize(view);
+      return new CartsVH(view);
+    }
+
+    public void update(List<CartsinfoBean> list) {
+
+      mList.addAll(list);
+      notifyDataSetChanged();
+    }
+
+    public void updateWithClear(List<CartsinfoBean> list) {
+      mList.clear();
+      mList.addAll(list);
+      notifyDataSetChanged();
+    }
+
+    @Override public void onBindViewHolder(final CartsVH holder, int position) {
+
+      CartsinfoBean cartsinfoBean = mList.get(position);
+      holder.title.setText(cartsinfoBean.getTitle());
+      holder.skuName.setText("尺码:" + cartsinfoBean.getSkuName());
+      //holder.color.setText(cartsinfoBean.get);
+      holder.price1.setText("¥" + cartsinfoBean.getPrice() + "");
+      holder.price2.setText("/¥" + cartsinfoBean.getStdSalePrice() + "");
+      holder.count.setText(cartsinfoBean.getNum() + "");
+
+      String headImg = cartsinfoBean.getPicPath();
+
+      ViewUtils.loadImgToImgView(mContext, holder.cartImage, headImg);
+
+      holder.add.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View v) {
+
+          model.plus_product_carts(cartsinfoBean.getId())
+              .subscribeOn(Schedulers.io())
+              .subscribe(new ServiceResponse<ResponseBody>() {
+                @Override public void onNext(ResponseBody responseBody) {
+                  super.onNext(responseBody);
+                  try {
+                    String s = responseBody.string();
+                    JUtils.Log("CartsAdapter", s);
+                    model.getCartsList()
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(new ServiceResponse<List<CartsinfoBean>>() {
+                          @Override public void onNext(List<CartsinfoBean> list) {
+                            super.onNext(list);
+                            mList = list;
+
+                            StringBuilder sb = new StringBuilder();
+                            String s = null;
+                            if (ids.size() > 0) {
+                              s = apendString(sb);
+                            }
+
+                            model.getCartsInfoList(s)
+                                .subscribeOn(Schedulers.newThread())
+                                .subscribe(new ServiceResponse<CartsPayinfoBean>() {
+                                  @Override
+                                  public void onNext(CartsPayinfoBean cartsPayinfoBean) {
+                                    super.onNext(cartsPayinfoBean);
+                                    if (cartsPayinfoBean != null) {
+
+                                      total_price = cartsPayinfoBean.getTotalFee();
+                                      totalPrice.setText("¥" + total_price);
+                                      totalPrice_all_1.setText("总金额¥" + total_price);
+
+                                      if (total_price < 150) {
+                                        extra_price = 150 - total_price;
+                                        extra_price_a.setText(extra_price+"");
+                                      }else {
+                                        extra_price_a.setText(0 + "");
+                                      }
+                                    }
+                                  }
+                                });
+
+                            notifyItemChanged(position);
+                          }
+                        });
+
+                    //holder.count.setText(cartsinfoBean.getNum() + "");
+                  } catch (IOException e) {
+                    e.printStackTrace();
+                  }
                 }
               });
-            }
-          }
-        });
+        }
+      });
 
-    totalPrice.setText(mCartsAdapetr.total_price + "");
+      holder.delete.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View v) {
+          if (Integer.parseInt(cartsinfoBean.getNum()) > 1) {
+
+            model.minus_product_carts(cartsinfoBean.getId())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new ServiceResponse<ResponseBody>() {
+                  @Override public void onNext(ResponseBody responseBody) {
+                    super.onNext(responseBody);
+
+                    model.getCartsList()
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(new ServiceResponse<List<CartsinfoBean>>() {
+                          @Override public void onNext(List<CartsinfoBean> list) {
+                            super.onNext(list);
+                            mList = list;
+
+                            StringBuilder sb = new StringBuilder();
+                            String s = null;
+                            if (ids.size() > 0) {
+                              s = apendString(sb);
+                            }
+
+                            model.getCartsInfoList(s)
+                                .subscribeOn(Schedulers.newThread())
+                                .subscribe(new ServiceResponse<CartsPayinfoBean>() {
+                                  @Override
+                                  public void onNext(CartsPayinfoBean cartsPayinfoBean) {
+                                    super.onNext(cartsPayinfoBean);
+                                    if (cartsPayinfoBean != null) {
+
+                                      total_price = cartsPayinfoBean.getTotalFee();
+                                      totalPrice.setText("¥" + total_price);
+                                      totalPrice_all_1.setText("总金额¥" + total_price);
+
+                                      if (total_price < 150) {
+                                        extra_price = 150 - total_price;
+                                        extra_price_a.setText(extra_price+"");
+                                      }else {
+                                        extra_price_a.setText(0 + "");
+                                      }
+                                    }
+                                  }
+                                });
+
+                            notifyItemChanged(position);
+                          }
+                        });
+                  }
+                });
+          } else {
+            new MaterialDialog.Builder(CartActivity.this).
+                title("删除商品").
+                content("您确定要删除吗？").
+                positiveText("确定").
+                negativeText("取消").
+                callback(new MaterialDialog.ButtonCallback() {
+                  @Override public void onPositive(MaterialDialog dialog) {
+                    model.delete_carts(cartsinfoBean.getId())
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(new ServiceResponse<ResponseBody>() {
+                          @Override public void onNext(ResponseBody responseBody) {
+                            super.onNext(responseBody);
+                            removeAt(position);
+                          }
+                        });
+                    dialog.dismiss();
+
+                    startActivity(new Intent(CartActivity.this, CartActivity.class));
+                    finish();
+                  }
+
+                  @Override public void onNegative(MaterialDialog dialog) {
+                    dialog.dismiss();
+                  }
+                }).show();
+          }
+        }
+      });
+    }
+
+    @Override public int getItemCount() {
+      return mList.size();
+    }
+
+    //删除某一项
+    public void removeAt(int position) {
+      mList.remove(position);
+      notifyItemRemoved(position);
+      notifyItemRangeChanged(position, mList.size());
+    }
+
+    class CartsVH extends RecyclerView.ViewHolder {
+      int id = R.layout.item_carts;
+      @Bind(R.id.cart_image) ImageView cartImage;
+      @Bind(R.id.title) TextView title;
+      @Bind(R.id.sku_name) TextView skuName;
+      //@Bind(R.id.color) TextView color;
+      @Bind(R.id.price1) TextView price1;
+      @Bind(R.id.price2) TextView price2;
+      @Bind(R.id.ll) LinearLayout ll;
+      @Bind(R.id.delete) TextView delete;
+      @Bind(R.id.count) TextView count;
+      @Bind(R.id.add) TextView add;
+
+      public CartsVH(View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+      }
+    }
   }
 }
