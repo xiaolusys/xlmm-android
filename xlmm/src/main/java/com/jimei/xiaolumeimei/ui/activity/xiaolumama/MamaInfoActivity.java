87c797c135eb5633d0319076ba380a5ec626571f
wulei <crackwl@163.com>
2016-02-17 19:55:24 +0800

    小鹿妈妈activity add chart

--- xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/xiaolumama/MamaInfoActivity.java
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/xiaolumama/MamaInfoActivity.java
@@ -1,13 +1,32 @@
 package com.jimei.xiaolumeimei.ui.activity.xiaolumama;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import butterknife.Bind;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.jimei.xiaolumeimei.R;
 import com.jimei.xiaolumeimei.base.BaseSwipeBackCompatActivity;
 import com.jimei.xiaolumeimei.entities.AgentInfoBean;
@@ -15,12 +34,14 @@ import com.jimei.xiaolumeimei.model.MamaInfoModel;
 import com.jimei.xiaolumeimei.ui.activity.main.MainActivity;
 import com.jimei.xiaolumeimei.xlmmService.ServiceResponse;
 import com.jude.utils.JUtils;
+import java.util.ArrayList;
 import rx.schedulers.Schedulers;
 
 /**
  * Created by wulei on 2016/2/4.
  */
-public class MamaInfoActivity extends BaseSwipeBackCompatActivity implements View.OnClickListener{
+public class MamaInfoActivity extends BaseSwipeBackCompatActivity implements View
+    .OnClickListener, OnChartGestureListener, OnChartValueSelectedListener {
   String TAG = "MamaInfoActivity";
 
   @Bind(R.id.toolbar) Toolbar toolbar;
@@ -30,6 +51,7 @@ public class MamaInfoActivity extends BaseSwipeBackCompatActivity implements Vie
   @Bind(R.id.tv_fansnum) TextView tv_fansnum;
   @Bind(R.id.btn_chooselist)Button btn_chooselist;
   @Bind(R.id.btn_store)Button btn_store;
+  @Bind(R.id.chart1) LineChart mChart;
 
   AgentInfoBean mamaAgentInfo;
 
@@ -57,6 +79,7 @@ public class MamaInfoActivity extends BaseSwipeBackCompatActivity implements Vie
     setSupportActionBar(toolbar);
     finishBack(toolbar);
 
+    init_chart();
   }
 
   @Override protected void initData() {
@@ -111,5 +134,184 @@ public class MamaInfoActivity extends BaseSwipeBackCompatActivity implements Vie
         startActivity(new Intent(MamaInfoActivity.this, MaMaMyStoreActivity.class));
         break;
     }
+  }
+
+  private void init_chart()
+  {
+
+    mChart.setOnChartGestureListener(this);
+    mChart.setOnChartValueSelectedListener(this);
+    mChart.setDrawGridBackground(false);
+
+    // no description text
+    mChart.setDescription("");
+    mChart.setNoDataTextDescription("You need to provide data for the chart.");
+
+    // enable touch gestures
+    mChart.setTouchEnabled(true);
+
+    // enable scaling and dragging
+    mChart.setDragEnabled(true);
+    mChart.setScaleEnabled(true);
+    // mChart.setScaleXEnabled(true);
+    // mChart.setScaleYEnabled(true);
+
+    // if disabled, scaling can be done on x- and y-axis separately
+    mChart.setPinchZoom(true);
+
+    // set an alternative background color
+    // mChart.setBackgroundColor(Color.GRAY);
+
+    // create a custom MarkerView (extend MarkerView) and specify the layout
+    // to use for it
+    //MyMarkerView mv = new MyMarkerView(this, R.layout.custom_marker_view);
+
+    // set the marker to the chart
+    //mChart.setMarkerView(mv);
+
+    // x-axis limit line
+    LimitLine llXAxis = new LimitLine(10f, "Index 10");
+    llXAxis.setLineWidth(4f);
+    llXAxis.enableDashedLine(10f, 10f, 0f);
+    llXAxis.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+    llXAxis.setTextSize(10f);
+
+    XAxis xAxis = mChart.getXAxis();
+
+    Typeface tf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
+
+    LimitLine ll1 = new LimitLine(130f, "Upper Limit");
+    ll1.setLineWidth(4f);
+    ll1.enableDashedLine(10f, 10f, 0f);
+    ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+    ll1.setTextSize(10f);
+    ll1.setTypeface(tf);
+
+    LimitLine ll2 = new LimitLine(-30f, "Lower Limit");
+    ll2.setLineWidth(4f);
+    ll2.enableDashedLine(10f, 10f, 0f);
+    ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+    ll2.setTextSize(10f);
+    ll2.setTypeface(tf);
+
+    YAxis leftAxis = mChart.getAxisLeft();
+    leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
+    leftAxis.addLimitLine(ll1);
+    leftAxis.addLimitLine(ll2);
+    leftAxis.setAxisMaxValue(220f);
+    leftAxis.setAxisMinValue(-50f);
+    //leftAxis.setYOffset(20f);
+    leftAxis.enableGridDashedLine(10f, 10f, 0f);
+    leftAxis.setDrawZeroLine(false);
+
+    // limit lines are drawn behind data (and not on top)
+    leftAxis.setDrawLimitLinesBehindData(true);
+
+    mChart.getAxisRight().setEnabled(false);
+
+    // add data
+    setData(45, 100);
+    mChart.animateX(2500, Easing.EasingOption.EaseInOutQuart);
+  }
+
+  private void setData(int count, float range) {
+
+    ArrayList<String> xVals = new ArrayList<String>();
+    for (int i = 0; i < count; i++) {
+      xVals.add((i) + "");
+    }
+
+    ArrayList<Entry> yVals = new ArrayList<Entry>();
+
+    for (int i = 0; i < count; i++) {
+
+      float mult = (range + 1);
+      float val = (float) (Math.random() * mult) + 3;// + (float)
+      // ((mult *
+      // 0.1) / 10);
+      yVals.add(new Entry(val, i));
+    }
+
+    // create a dataset and give it a type
+    LineDataSet set1 = new LineDataSet(yVals, "DataSet 1");
+    // set1.setFillAlpha(110);
+    // set1.setFillColor(Color.RED);
+
+    // set the line to be drawn like this "- - - - - -"
+    set1.enableDashedLine(10f, 5f, 0f);
+    set1.enableDashedHighlightLine(10f, 5f, 0f);
+    set1.setColor(Color.BLACK);
+    set1.setCircleColor(Color.BLACK);
+    set1.setLineWidth(1f);
+    set1.setCircleRadius(3f);
+    set1.setDrawCircleHole(false);
+    set1.setValueTextSize(9f);
+    Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
+    set1.setFillDrawable(drawable);
+    set1.setDrawFilled(true);
+
+    ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+    dataSets.add(set1); // add the datasets
+
+    // create a data object with the datasets
+    LineData data = new LineData(xVals, dataSets);
+
+    // set data
+    mChart.setData(data);
+  }
+
+  @Override
+  public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+    Log.i("Gesture", "START");
+  }
+
+  @Override
+  public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+    Log.i("Gesture", "END, lastGesture: " + lastPerformedGesture);
+
+    // un-highlight values after the gesture is finished and no single-tap
+    if(lastPerformedGesture != ChartTouchListener.ChartGesture.SINGLE_TAP)
+      mChart.highlightValues(null); // or highlightTouch(null) for callback to onNothingSelected(...)
+  }
+
+  @Override
+  public void onChartLongPressed(MotionEvent me) {
+    Log.i("LongPress", "Chart longpressed.");
+  }
+
+  @Override
+  public void onChartDoubleTapped(MotionEvent me) {
+    Log.i("DoubleTap", "Chart double-tapped.");
+  }
+
+  @Override
+  public void onChartSingleTapped(MotionEvent me) {
+    Log.i("SingleTap", "Chart single-tapped.");
+  }
+
+  @Override
+  public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+    Log.i("Fling", "Chart flinged. VeloX: " + velocityX + ", VeloY: " + velocityY);
+  }
+
+  @Override
+  public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+    Log.i("Scale / Zoom", "ScaleX: " + scaleX + ", ScaleY: " + scaleY);
+  }
+
+  @Override
+  public void onChartTranslate(MotionEvent me, float dX, float dY) {
+    Log.i("Translate / Move", "dX: " + dX + ", dY: " + dY);
+  }
+
+  @Override
+  public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
+    Log.i("Entry selected", e.toString());
+    Log.i("", "low: " + mChart.getLowestVisibleXIndex() + ", high: " + mChart.getHighestVisibleXIndex());
+  }
+
+  @Override
+  public void onNothingSelected() {
+    Log.i("Nothing selected", "Nothing selected.");
   }
 }
