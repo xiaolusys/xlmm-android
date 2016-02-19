09451ce226d2e1c0700803cebb10d796044c3043
lei.wu <crackwl@163.com>
2016-02-19 18:10:22 +0800

    小鹿妈妈ui fix

--- xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/xiaolumama/TwoDimenCodeActivity.java
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/xiaolumama/TwoDimenCodeActivity.java
@@ -81,11 +81,11 @@ public class TwoDimenCodeActivity extends BaseSwipeBackCompatActivity implements
     // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
     //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
     // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-    oks.setTitle("test");
+    //oks.setTitle("test");
     // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
     //oks.setTitleUrl("http://sharesdk.cn");
     // text是分享文本，所有平台都需要这个字段
-    oks.setText("我是分享文本");
+    oks.setText("分享我的二维码");
     // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
     //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
     // url仅在微信（包括好友和朋友圈）中使用
