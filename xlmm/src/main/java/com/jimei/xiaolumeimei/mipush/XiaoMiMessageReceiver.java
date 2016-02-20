f7632e8d70ea07ac675cc70ca62b5e16c2893808
wulei <crackwl@163.com>
2016-02-20 22:49:25 +0800

    xiaomi push proc add

--- xlmm/src/main/java/com/jimei/xiaolumeimei/mipush/XiaoMiMessageReceiver.java
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/mipush/XiaoMiMessageReceiver.java
@@ -132,6 +132,21 @@ public class XiaoMiMessageReceiver extends PushMessageReceiver {
       if (message.getResultCode() == ErrorCode.SUCCESS) {
         mRegId = cmdArg1;
         log = context.getString(R.string.register_success);
+
+        //register xiaomi push
+        JUtils.Log("XlmmApp", "regid: " + mRegId
+            + " devid:"+((TelephonyManager) context.getSystemService( Context.TELEPHONY_SERVICE ))
+            .getDeviceId());
+        new UserModel().getUserAccount("android", mRegId,
+            ((TelephonyManager)context.getSystemService( Context.TELEPHONY_SERVICE ))
+                .getDeviceId())
+            .subscribeOn(Schedulers.newThread())
+            .subscribe(new ServiceResponse<UserAccountBean>() {
+              @Override public void onNext(UserAccountBean user) {
+                JUtils.Log("XlmmApp", "UserAccountBean:, " + user.toString());
+                MiPushClient.setUserAccount(context.getApplicationContext(), user.getUserAccount(), null);
+              }
+            });
       } else {
         log = context.getString(R.string.register_fail);
       }
