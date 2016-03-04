17cd9778ff296255a8d2398e5c42e495addb3485
itxuye <itxuye@gmail.com>
2016-03-04 13:30:32 +0800

    提交分享修改

--- xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/xiaolumama/MaMaMyStoreActivity.java
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/xiaolumama/MaMaMyStoreActivity.java
@@ -43,6 +43,7 @@ public class MaMaMyStoreActivity extends BaseSwipeBackCompatActivity {
   @Bind(R.id.loading) RotateLoading loading;
   @Bind(R.id.titleiamge) CircleImageView titleiamge;
   private MaMaStoreAdapter maMaStoreAdapter;
+  private String title, sharelink, desc, shareimg;
 
   @Override protected void setListener() {
 
@@ -73,6 +74,22 @@ public class MaMaMyStoreActivity extends BaseSwipeBackCompatActivity {
         });
 
     addSubscription(subscribe);
+
+    MMProductModel.getInstance()
+        .getShareShopping()
+        .subscribeOn(Schedulers.io())
+        .subscribe(new ServiceResponse<MMShoppingBean>() {
+
+          @Override public void onNext(MMShoppingBean mmShoppingBean) {
+
+            if (null != mmShoppingBean) {
+              title = (String) mmShoppingBean.getShopInfo().getName();
+              sharelink = mmShoppingBean.getShopInfo().getShopLink();
+              shareimg = mmShoppingBean.getShopInfo().getThumbnail();
+              desc = mmShoppingBean.getShopInfo().getDesc();
+            }
+          }
+        });
   }
 
   @Override protected void getBundleExtras(Bundle extras) {
@@ -154,50 +171,28 @@ public class MaMaMyStoreActivity extends BaseSwipeBackCompatActivity {
     }
 
     if (item.getItemId() == R.id.action_share) {
-
-      MMProductModel.getInstance()
-          .getShareShopping()
-          .subscribeOn(Schedulers.io())
-          .subscribe(new ServiceResponse<MMShoppingBean>() {
-            @Override public void onNext(MMShoppingBean mmShoppingBean) {
-              if (null != mmShoppingBean) {
-                mmShoppingBean.getShopInfo().getShopLink();
-                share_shopping();
-              }
-            }
-          });
+      share_shopping(title, sharelink, desc, shareimg);
     }
 
     return super.onOptionsItemSelected(item);
   }
 
-  private void share_shopping() {
+  private void share_shopping(String title, String sharelink, String desc,
+      String shareimg) {
     OnekeyShare oks = new OnekeyShare();
     //关闭sso授权
     oks.disableSSOWhenAuthorize();
 
-    // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
-    //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
-    // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-    //oks.setTitle(shareProductBean.getTitle());
-    //// titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-    //oks.setTitleUrl(shareProductBean.getShareLink());
-    //// text是分享文本，所有平台都需要这个字段
-    //oks.setText(shareProductBean.getDesc());
-    //// imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-    ////oks.setImagePath(filePara.getFilePath());//确保SDcard下面存在此张图片
-    ////oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
-    //oks.setImageUrl(shareProductBean.getShareImg());
-    //oks.setUrl(shareProductBean.getShareLink());
-
-    // url仅在微信（包括好友和朋友圈）中使用
-    //oks.setUrl(myurl);
-    // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-    //oks.setComment("我是测试评论文本");
-    // site是分享此内容的网站名称，仅在QQ空间使用
-    //oks.setSite(getString(R.string.app_name));
-    // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-    //oks.setSiteUrl("http://sharesdk.cn");
+    oks.setTitle(title);
+    // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+    oks.setTitleUrl(sharelink);
+    // text是分享文本，所有平台都需要这个字段
+    oks.setText(desc);
+    // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+    //oks.setImagePath(filePara.getFilePath());//确保SDcard下面存在此张图片
+    //oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+    oks.setImageUrl(shareimg);
+    oks.setUrl(sharelink);
 
     // 启动分享GUI
     oks.show(this);
