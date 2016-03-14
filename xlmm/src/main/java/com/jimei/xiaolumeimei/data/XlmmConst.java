a13016d388cf6b307934af5bc26d1391aedfae79
wulei <crackwl@163.com>
2016-03-14 09:42:48 +0800

    change version

--- xlmm/src/main/java/com/jimei/xiaolumeimei/data/XlmmConst.java
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/data/XlmmConst.java
@@ -5,7 +5,7 @@ package com.jimei.xiaolumeimei.data;
  */
 public class XlmmConst {
 
-    public static final String VERSION = "1.0.6";
+    public static final String VERSION = "1.0.6.9";
 
     public static  final int ORDER_STATE_CREATE = 0;
     public static  final int ORDER_STATE_WAITPAY = 1;
