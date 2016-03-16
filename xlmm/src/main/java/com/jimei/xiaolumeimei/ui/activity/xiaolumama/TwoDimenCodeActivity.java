fb311b223a2d4cd1861ea204c4da65338929d05e
wulei <crackwl@163.com>
2016-03-16 21:04:52 +0800

    qrcode 分享bug fix

--- xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/xiaolumama/TwoDimenCodeActivity.java
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/xiaolumama/TwoDimenCodeActivity.java
@@ -136,14 +136,14 @@ public class TwoDimenCodeActivity extends BaseSwipeBackCompatActivity
     // text是分享文本，所有平台都需要这个字段
     oks.setText("分享我的二维码");
     // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-    if ((filePara != null) && (filePara.getFilePath() != null) && (!filePara.getFilePath()
+    /*if ((filePara != null) && (filePara.getFilePath() != null) && (!filePara.getFilePath()
         .isEmpty())) {
       oks.setImagePath(filePara.getFilePath());//确保SDcard下面存在此张图片
       JUtils.Log(TAG, "local pic " + filePara.getFilePath());
-    } else {
+    } else {*/
       JUtils.Log(TAG, "url pic");
       oks.setImageUrl(myurl);
-    }
+    //}
     // url仅在微信（包括好友和朋友圈）中使用
     //oks.setUrl(myurl);
     // comment是我对这条分享的评论，仅在人人网和QQ空间使用
