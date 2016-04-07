dded8085dffeb3ab2fddce24bb613c36b6aab9fc
itxuye <itxuye@gmail.com>
2016-04-07 11:33:13 +0800

    解决分割线在footer也会显示

---
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/web/CommonWebViewNoShareActivity.java
@@ -0,0 +1,45 @@
+package com.jimei.xiaolumeimei.ui.activity.web;
+
+import android.os.Bundle;
+
+import com.jimei.xiaolumeimei.base.BaseSwipeBackCompatActivity;
+
+/**
+ * Created by itxuye(http://www.itxuye.com) on 16/4/7.
+ */
+public class CommonWebViewNoShareActivity extends BaseSwipeBackCompatActivity{
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void getBundleExtras(Bundle extras) {
+
+    }
+
+    @Override
+    protected int getContentViewLayoutID() {
+        return 0;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected boolean toggleOverridePendingTransition() {
+        return false;
+    }
+
+    @Override
+    protected TransitionMode getOverridePendingTransitionMode() {
+        return null;
+    }
+}
