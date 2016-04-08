e48482ea8b138c68cb277f97aaa61a43088a3362
itxuye <itxuye@gmail.com>
2016-04-08 17:32:27 +0800

    carts change

--- xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/trade/CartActivity.java
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/trade/CartActivity.java
@@ -13,8 +13,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import butterknife.Bind;
-import butterknife.ButterKnife;
+
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.jimei.xiaolumeimei.R;
 import com.jimei.xiaolumeimei.base.BaseSwipeBackCompatActivity;
@@ -30,9 +29,13 @@ import com.jimei.xiaolumeimei.xlmmService.ServiceResponse;
 import com.jude.utils.JUtils;
 import com.squareup.okhttp.ResponseBody;
 import com.zhy.autolayout.utils.AutoUtils;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import rx.Subscription;
 import rx.schedulers.Schedulers;
 
@@ -91,19 +94,26 @@ public class CartActivity extends BaseSwipeBackCompatActivity
     extra_price_a = (TextView) view.findViewById(R.id.extra_price_a);
     haicha = (TextView) view.findViewById(R.id.haicha);
 
+    cartsRecyclerview.setNestedScrollingEnabled(false);
+    cartsRecyclerview.setHasFixedSize(false);
     cartsRecyclerview.addItemDecoration(
         new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
-
-    cartsRecyclerview.setLayoutManager(new ScrollLinearLayoutManager(this));
+    ScrollLinearLayoutManager scrollLinearLayoutManager = new ScrollLinearLayoutManager(this);
+    scrollLinearLayoutManager.setAutoMeasureEnabled(true);
+    cartsRecyclerview.setLayoutManager(scrollLinearLayoutManager);
 
     mCartsAdapetr = new CartsAdapetr();
 
     cartsRecyclerview.setAdapter(mCartsAdapetr);
 
+    cartshisRecyclerview.setNestedScrollingEnabled(false);
+    cartshisRecyclerview.setHasFixedSize(false);
+
     cartshisRecyclerview.addItemDecoration(
         new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
-
-    cartshisRecyclerview.setLayoutManager(new ScrollLinearLayoutManager(this));
+    ScrollLinearLayoutManager scrollLinearLayoutManager1 = new ScrollLinearLayoutManager(this);
+    scrollLinearLayoutManager1.setAutoMeasureEnabled(true);
+    cartshisRecyclerview.setLayoutManager(scrollLinearLayoutManager1);
 
     mCartsHisAdapetr = new CartsHisAdapetr();
 
@@ -167,13 +177,13 @@ public class CartActivity extends BaseSwipeBackCompatActivity
                         mCartsHisAdapetr.updateWithClear(cartsinfoBeen);
                       } else {
 
-                          emptyContent.setVisibility(View.VISIBLE);
+                         /* emptyContent.setVisibility(View.VISIBLE);
                           goMain.setOnClickListener(new View.OnClickListener() {
                             @Override public void onClick(View v) {
                               startActivity(new Intent(CartActivity.this, MainActivity.class));
                               finish();
                             }
-                          });
+                          });*/
 
                         tvShow.setVisibility(View.INVISIBLE);
                         showLine.setVisibility(View.INVISIBLE);
@@ -209,6 +219,32 @@ public class CartActivity extends BaseSwipeBackCompatActivity
                     }
                   });
               addSubscription(subscription1);
+            }else {
+
+              Subscription subscribe = CartsModel.getInstance()
+                      .getCartsHisList()
+                      .subscribeOn(Schedulers.io())
+                      .subscribe(new ServiceResponse<List<CartsinfoBean>>() {
+                        @Override public void onNext(List<CartsinfoBean> cartsinfoBeen) {
+                          if (null != cartsinfoBeen) {
+                            mCartsHisAdapetr.updateWithClear(cartsinfoBeen);
+                          } else {
+
+                            emptyContent.setVisibility(View.VISIBLE);
+                            goMain.setOnClickListener(new View.OnClickListener() {
+                              @Override public void onClick(View v) {
+                                startActivity(new Intent(CartActivity.this, MainActivity.class));
+                                finish();
+                              }
+                            });
+
+                            tvShow.setVisibility(View.INVISIBLE);
+                            showLine.setVisibility(View.INVISIBLE);
+                          }
+                        }
+                      });
+              addSubscription(subscribe);
+
             }
 
             //else {
@@ -482,12 +518,6 @@ public class CartActivity extends BaseSwipeBackCompatActivity
                         totalPrice.setText("¥" + total_price);
                         totalPrice_all_1.setText("总金额¥" + total_price);
 
-                        if (total_price < 150) {
-                          extra_price = 150 - total_price;
-                          extra_price_a.setText("还差" + extra_price + "元");
-                        } else {
-                          haicha.setVisibility(View.INVISIBLE);
-                        }
                       }
                     }
                   });
