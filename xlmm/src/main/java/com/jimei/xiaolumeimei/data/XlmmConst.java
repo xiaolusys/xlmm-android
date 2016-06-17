d9da073fbbf56e26ec894cef4f144902d4d0efa4
wisdom <1131027649@qq.com>
2016-06-17 16:36:27 +0800

    refundstatus

--- xlmm/src/main/java/com/jimei/xiaolumeimei/data/XlmmConst.java
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/data/XlmmConst.java
@@ -5,7 +5,7 @@ package com.jimei.xiaolumeimei.data;
  */
 public class XlmmConst {
 
-    public static final String VERSION = "1.1.5";
+    public static final String VERSION = "1.1.6";
 
     public static  final int ORDER_STATE_CREATE = 0;
     public static  final int ORDER_STATE_WAITPAY = 1;
