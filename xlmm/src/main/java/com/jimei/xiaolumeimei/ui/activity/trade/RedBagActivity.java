4e30b5bde68f8c221cafea77ecc59a41274cb959
wisdom <1131027649@qq.com>
2016-06-22 09:03:18 +0800

    1.1.6.1

---
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/ui/activity/trade/RedBagActivity.java
@@ -0,0 +1,44 @@
+package com.jimei.xiaolumeimei.ui.activity.trade;
+
+import android.os.Bundle;
+
+import com.jimei.xiaolumeimei.R;
+import com.jimei.xiaolumeimei.base.BaseSwipeBackCompatActivity;
+
+public class RedBagActivity extends BaseSwipeBackCompatActivity {
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void getBundleExtras(Bundle extras) {
+
+    }
+
+    @Override
+    protected int getContentViewLayoutID() {
+        return R.layout.activity_red_bag;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected boolean toggleOverridePendingTransition() {
+        return false;
+    }
+
+    @Override
+    protected TransitionMode getOverridePendingTransitionMode() {
+        return null;
+    }
+}
