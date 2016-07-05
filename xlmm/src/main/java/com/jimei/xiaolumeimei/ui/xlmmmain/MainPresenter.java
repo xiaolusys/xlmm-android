80ad8f8af3a82e0eaf074e20cf75eacc9cdd445e
itxuye <itxuye@gmail.com>
2016-07-05 09:34:28 +0800

    部分界面修改。main  mvp初步

---
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/ui/xlmmmain/MainPresenter.java
@@ -0,0 +1,10 @@
+package com.jimei.xiaolumeimei.ui.xlmmmain;
+
+/**
+ * Created by itxuye on 2016/7/4.
+ */
+public class MainPresenter extends MainContract.Presenter {
+  @Override public void onStart() {
+
+  }
+}
