6446e586f67178b47e508aa7dd16fd00f30f4efd
wisdom <1131027649@qq.com>
2017-03-29 09:56:26 +0800

    recyclerview内部错误修复

---
+++ xlmm/src/main/java/com/jimei/xiaolumeimei/adapter/CustomLinearLayoutManager.java
@@ -0,0 +1,25 @@
+package com.jimei.xiaolumeimei.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+/**
+ * Created by wisdom on 17/3/28.
+ */
+
+public class CustomLinearLayoutManager extends LinearLayoutManager {
+    public CustomLinearLayoutManager(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
+        try {
+            super.onLayoutChildren(recycler, state);
+        } catch (IndexOutOfBoundsException e) {
+            Log.e("probe", "meet a IOOBE in RecyclerView");
+        }
+    }
+}
